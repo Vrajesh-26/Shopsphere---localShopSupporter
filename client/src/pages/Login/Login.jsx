39d4101deb947a3bec3aefbe3baa8324ebba78b9
Vrajesh-26 <vrajdarji2611@gmail.com>
2025-10-04 23:11:17 +0530

    Add login frontend

---
+++ client/src/pages/Login/Login.jsx
@@ -0,0 +1,86 @@
+import { useContext, useState } from 'react';
+import './Login.css';
+import toast from 'react-hot-toast';
+import { login } from '../../Service/AuthService';
+import { useNavigate } from 'react-router-dom';
+import { AppContext } from '../../context/AppContext';
+
+const Login = () => {
+
+    const {setAuthData} = useContext(AppContext);
+    // Loading states 
+    const [loading, setLoading] = useState(false);
+    //Set data 
+    const [data, setData] = useState({
+        email: "",
+        password: ""
+    });
+
+    const onChangeHandler = (e) => {
+        const navigate = useNavigate();
+        const name = e.target.name;
+        const value = e.target.value;
+        setData((data) => ({...data, [name]: value}));
+    }
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefault(); // its stop reloading web page 
+        setLoading(true); // Loadind set true so it re-render all component anywhere use this.
+        try{
+            const response = await login(data);
+            if(response.status === 200){
+                toast.success("Login successfully");
+                localStorage.setItem("token", response.data.token); // When jws & role of user token comes it store it locally in device 
+                localStorage.setItem("role", response.data.role);
+                setAuthData(response.data.token, response.data.role); // set token comes from backend
+                navigate("/dashboard");
+            }
+        }catch(e){
+            console.error(e);
+            toast.error("Email/Password Invalid");
+        }finally{
+            setLoading(false);
+        }
+    }
+
+    return(
+        <div className="bg-light d-flex align-items-center justify-content-center vh-100 login-background">
+            <div className="card shadow-lg w-100" style={{maxWidth: '480px'}}>
+                <div className="card-body">
+                    <div className="text-center">
+                        <h1 className="card-title">Sign in</h1>
+                        <p className="card-text text-muted">
+                            Sign in below to access your account
+                        </p>
+                    </div>
+
+                    <div className="mt-4">
+                        <form onSubmit={onSubmitHandler}>
+                            <div className="mb-4">
+                                <label htmlFor="email" className="form-label text-muted">
+                                    Email address 
+                                </label>
+                                <input type="text" name="email" id="email" placeholder="yourname@example.com" className="form-control" onChange={onChangeHandler} value={data.email}/>
+                            </div>
+
+                            <div className="mb-4">
+                                 <label htmlFor="password" className="form-label text-muted">
+                                    Password
+                                </label>
+                                <input type="password" name="password" id="password" placeholder="************" className="form-control" onChange={onChangeHandler} value={data.password}/>
+                            </div>
+
+                            <div className="d-grid">
+                                <button type="submit" className="btn btn-dark btn-lg">
+                                    Sign in
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div> 
+            </div>
+        </div>
+    )
+}
+
+export default Login;
