51cbca863caada67ecfe4c2a42bde8cf500fc7cd
Vrajesh-26 <vrajdarji2611@gmail.com>
2025-10-04 23:11:17 +0530

    Add login frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Menubar from "./components/Menubar/Menubar.jsx";
 import Dashboard from "./pages/Dashboard/Deshbord.jsx";
 import ManageCategory from "./pages/ManageCategory/ManageCategory.jsx";
@@ -6,20 +6,28 @@ import ManageUsers from "./pages/ManageUsers/ManageUsers.jsx";
 import ManageItems from "./pages/ManageItems/ManageItems.jsx";
 import Explore from "./pages/Explore/Explore.jsx";
 import { Toaster } from "react-hot-toast";
+import Login from "./pages/Login/login.jsx";
 
 const App = () => {
-    return(<div>
-        <Menubar />
-        <Toaster />
-        <Routes>
-            <Route path= "/deshboard" element= {<Dashboard />} />
-            <Route path= "/category" element= {<ManageCategory />} />
-            <Route path= "/users" element= {<ManageUsers />} />            
-            <Route path= "/items" element= {<ManageItems />} />
-            <Route path= "/explore" element= {<Explore />} />
-            <Route path= "/" element= {<Dashboard />} />
-        </Routes>
-    </div>);
+    //Location hook
+    const location = useLocation();
+
+    return(
+
+        <div>
+            {location.pathname !== "/login" && <Menubar />}
+            <Toaster />
+            <Routes>
+                <Route path= "/deshboard" element= {<Dashboard />} />
+                <Route path= "/category" element= {<ManageCategory />} />
+                <Route path= "/users" element= {<ManageUsers />} />            
+                <Route path= "/items" element= {<ManageItems />} />
+                <Route path= "/explore" element= {<Explore />} />
+                <Route path= "/login" element= {<Login />} />
+                <Route path= "/" element= {<Dashboard />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -6,6 +6,10 @@ export const AppContext = createContext(null);
 export const AppContextProvider = (props) => {
 
     const [categories, setCategories] = useState([]);
+
+
+    //set authenticaion credential 
+    const [auth, setAuth] = useState({token: null, role: null});
     
     useEffect(() => {
         async function loadData(){
@@ -15,9 +19,16 @@ export const AppContextProvider = (props) => {
         loadData();
     }, []);
 
+    // Not pass direct response from login api we set on this variable then this state pass 
+    const setAuthData = (token, role) => {
+        setAuth({token, role});
+    } 
+
     const contextValue = {
         categories,
-        setCategories
+        setCategories,
+        auth,
+        setAuthData
     }
 
     return <AppContext.Provider value={contextValue}>
