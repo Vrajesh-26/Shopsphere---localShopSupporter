9b8ebcf3014e2497ea2edb8059b0b45ae737a391
Vrajesh-26 <vrajdarji2611@gmail.com>
2025-10-05 21:36:11 +0530

    Adding login/logout frontend

--- client/src/components/ItemForm/ItemForm.jsx
+++ client/src/components/ItemForm/ItemForm.jsx
@@ -1,15 +1,81 @@
+import { useContext, useState } from 'react';
+import './ItemForm.css';
+import { assets } from '../../assets/assets';
+import {AppContext} from '../../context/AppContext';
+import { addItem } from '../../Service/ItemService';
+import toast from 'react-hot-toast';
+
 const ItemForm = ({ item, onChange}) => {
+
+    const {categories, setItemsData, itemsData, setCategories} = useContext(AppContext);
+
+    //Createing states
+    const [image, setImage] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState({
+        name: "",
+        categoryId: "",
+        description:"",
+        price:"",
+    });
+
+    //Create handler 
+    const onChangeHandler = (e) =>{
+        const value = e.target.value;
+        const name = e.target.name;
+        setData((data) => ({...data, [name]: value}));
+    }
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        const formData = new FormData();  // Create form for data having image and JSOM form data
+        formData.append("item", JSON.stringify(data));
+        formData.append("file", image);
+        try{
+            if(!image){
+                toast.error("Select image");
+                return;
+            }
+            const response = await addItem(formData);
+            
+            if(response.status === 201){
+                setItemsData([...itemsData, response.data]);
+                //Setting all category's item number then match it true add 1
+                setCategories((prevCategories) => 
+                prevCategories.map((category) => category.categoryId === data.categoryId ? {...category, items: category.items + 1 } : category));
+                toast.success("Item added");
+                setData({
+                    name: "",
+                    description: "",
+                    price:"",
+                    categoryId: ""
+                });
+                setImage(false); //reset image upload option
+            }else{
+                toast.error("Unable to add item");
+            } 
+        }catch(e){
+            console.error(e);
+            toast.error("Unable to add item");
+        }finally{
+            setLoading(false);
+        }
+    }
+
+
+
     return (
-        <div className="item-form-container" style={{height:'100vh', overflowY: 'auto', overflowX: 'h'}}>
+        <div className="item-form-container" style={{height:'100vh', overflowY: 'auto', overflowX: 'hidden'}}>
             <div className="mx-2 mt-2">
                 <div className="row">
                     <div className="card col-md-8 form-container">
                         <div className="card-body">
-                            <form>
+                            <form onSubmit={onSubmitHandler}>
                                 <div className="mb-3">
                                     <label htmlFor="image" className="form-lable">
-                                        <img src="https://placehold.co/48x48" alt="" width={48}/>
-                                        <input type="file" name="image" id="image" className="form-control" hidden/>
+                                        <img src={image ? URL.createObjectURL(image) : assets.upload} alt="" width={48}/>
+                                        <input type="file" name="image" id="image" className="form-control" hidden onChange={(e) => setImage(e.target.files[0])}/>
                                     </label>
                                 </div>
 
@@ -20,6 +86,8 @@ const ItemForm = ({ item, onChange}) => {
                                         id="name"
                                         className="form-control"
                                         placeholder="Item Name"
+                                        onChange={onChangeHandler}
+                                        value={data.name}
                                     />
                                 </div>
 
@@ -28,34 +96,39 @@ const ItemForm = ({ item, onChange}) => {
                                         Category
                                     </label>
 
-                                    <select name="category" id="category" className="form-control">
+                                    <select name="categoryId" id="categoryId" className="form-control" onChange={onChangeHandler} value={data.categoryId}>
                                         <option value="">--SELECT CATEGORY--</option>
-                                        <option value="Category 1">category 1</option>
-                                        <option value="Category 2">category 1</option>
+                                        {categories.map((category, index) => (
+                                            <option key={index} value={category.categoryId}>{category.name}</option>
+                                        ))};
                                     </select>
                                 </div>
 
                                 <div className="mb-3">
                                     <label htmlFor="price" className="form-label">Price</label>
-                                    <input type="number" name="price" id="price" className="form-control" placeholder="&#8377;200.00"></input>
+                                    <input type="number" name="price" id="price" className="form-control" placeholder="&#8377;200.00" onChange={onChangeHandler} value={data.price}></input>
                                 </div>
 
                                 <div className="mb-3">
                                     <label htmlFor="description" className="form-label">Description</label>
                                     <textarea 
                                         rows='5' 
-                                        name="name"
+                                        name="description"
                                         id="description"
                                         className="form-control"
                                         placeholder="Write content here.."
+                                        onChange={onChangeHandler}
+                                        value={data.description}
                                     ></textarea>
                                 </div>
 
-                                <button type="submit" className="btn btn-warning w-100">Save</button>
+                                <button type="submit" className="btn btn-warning w-100" disabled={loading}>
+                                    {loading? "Loading..." : "Save"}
+                                </button>
                             </form>
                         </div>
                     </div>
-                </div>
+                </div> 
             </div>
         </div>
     )
--- client/src/components/Menubar/Menubar.jsx
+++ client/src/components/Menubar/Menubar.jsx
@@ -1,8 +1,22 @@
 import './Menubar.css';
 import {assets} from '../../assets/assets.js';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { AppContext } from '../../context/AppContext.jsx';
 
 const Menubar = () => {
+    const navigate = useNavigate();
+    //Get auth credential 
+    const {setAuthData} = useContext(AppContext);
+
+    //handler for logout 
+    const logout = () => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("role");
+        setAuthData(null, null); // reset 
+        navigate("/login");
+    }
+
     return(
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-2">
     <a className="navbar-brand" href="#">
@@ -28,7 +42,29 @@ const Menubar = () => {
             <li className="nav-item">
                 <Link className="nav-link" to="/users">Manage Users</Link>
             </li>
-            {/* Add the dropdown for userprofile */}
+            <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+                <li className="nav-item dropdown">
+                    <a href="#" className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src={assets.profile} alt="" height={32} width={32} />
+                    </a>
+                    <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li>
+                            <a href="#" className="dropdown-item">
+                                Settings
+                            </a>
+                            <a href="#" className="dropdown-item">
+                                Activity log
+                            </a>
+                            <li>
+                                <hr className="dropdown-divider" />
+                            </li>
+                            <a href="#" className="dropdown-item" onClick={logout}>
+                                Logout
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
         </ul>
     </div>
 </nav>
