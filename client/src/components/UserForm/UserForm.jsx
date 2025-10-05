b9a2afe750963747eb835f07848e7c98049fb70d
Vrajesh-26 <vrajdarji2611@gmail.com>
2025-10-05 16:01:55 +0530

    Completing user part from frontend

--- client/src/components/UserForm/UserForm.jsx
+++ client/src/components/UserForm/UserForm.jsx
@@ -1,17 +1,61 @@
-const UserForm = () => {
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { addUser } from "../../Service/UserService";
+
+const UserForm = ({setUsers}) => {
+
+    //Creates states 
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState({  // Initial state 
+        name: "",
+        email: "",
+        password: "",
+        role: "ROLE_USER"
+    });
+
+    //Adding handler
+    const onChangeHandler = (e) => {
+        const value = e.target.value;
+        const name = e.target.name;
+        setData((data) => ({...data, [name]: value}));
+    }
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        try{
+            const response = await addUser(data);
+            setUsers((prevUsers) => [...prevUsers, response.data]);
+            toast.success("User Added");
+            setData({               //Set again blank form to add new users not reload
+                name: "",
+                email: "",
+                password: "",
+                role: "ROLE_USER"
+            });
+        }catch(e){
+            console.log(e)
+            toast.error("Error adding user");
+        }finally{
+            setLoading(false);
+        }
+    }
+
     return(
             <div className="mx-2 mt-2">
             <div className="row">
-                <div className="card col-md-8 form-container">
+                <div className="card col-md-12 form-container">
                     <div className="card-body">
-                        <form>
+                        <form onSubmit={onSubmitHandler}>
                             <div className="mb-3">
                                 <label htmlFor="name" className="form-label">Name</label>
                                 <input type="text" 
                                     name="name"
                                     id="name"
                                     className="form-control"
-                                    placeholder="Jhon Doe"
+                                    placeholder="name"
+                                    onChange={onChangeHandler}
+                                    value={data.name}
                                 />
                             </div>
 
@@ -22,6 +66,8 @@ const UserForm = () => {
                                     id="email"
                                     className="form-control"
                                     placeholder="yourname@example.com"
+                                    onChange={onChangeHandler}
+                                    value={data.email}
                                 />
                             </div>
 
@@ -32,10 +78,14 @@ const UserForm = () => {
                                     id="password"
                                     className="form-control"
                                     placeholder="****************"
+                                    onChange={onChangeHandler}
+                                    value={data.password}
                                 />
                             </div>
 
-                            <button type="submit" className="btn btn-warning w-100">Save</button>
+                            <button type="submit" className="btn btn-warning w-100" disabled={loading}>
+                                {loading? "Loading.. " : "Save"}
+                            </button>
                         </form>
                     </div>
                 </div>
